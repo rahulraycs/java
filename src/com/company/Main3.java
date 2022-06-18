d2d1220900d6f339c8c4dc258bc1f0930668473e
rahulraycs <rahulray2103@gmail.com>
2022-06-18 23:13:10 +0530

    Java Practice 2

---
+++ src/com/company/Main3.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Parrot p = new Parrot("mittu");
+        p.breathe();
+        p.fly();
+        p.eat();
+    }
+}
